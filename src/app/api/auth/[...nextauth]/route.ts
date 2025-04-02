5beb8cf4d672b4a7a665b00ca0c97de7fbfaf5c5
Nitish Kumar <kumarnitish4384@gmail.com>
2025-04-02 19:39:59 +0530

    done

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
-import { authOptions } from "@/lib/auth"; // ✅ Correct Import
-
+import { authOptions } from "@/lib/auth"; 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST }; // ✅ NextAuth v4 ke liye yeh syntax hai
+export { handler as GET, handler as POST }; 
